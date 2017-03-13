6d68d3fda04aa77796cf069ce12a349a0fe5bcf6
xiaowa <xiaowa@youzan.com>
2017-03-13 14:56:24 +0800

    处理日期

--- pykylin/transfer.py
+++ pykylin/transfer.py
@@ -6,8 +6,10 @@ from .log import logger
 
 field_pattern = r"(\s+)(.*) AS ([a-z_]*)"
 group_py_pattern = r"GROUP BY (.*)\s"
+where_pattern = r"(WHERE|AND|OR)\s+(.*?)\s+"
 
 table_pattern = r"FROM (.*?)\s"
+date_pattern = r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}"
 
 
 def transfer_sql(sql):
@@ -16,9 +18,19 @@ def transfer_sql(sql):
         return sql
     sql = re.sub(field_pattern, lambda x: transfer_field(x, table), sql)
     sql = re.sub(group_py_pattern, lambda x: transfer_group_by(x, table), sql)
+    sql = re.sub(where_pattern, lambda x: transfer_where(x, table), sql)
+    sql = re.sub(date_pattern, transfer_date, sql)
 
     sql = sql.upper()
     return sql
+
+
+def transfer_date(date_sql):
+    date_sql = date_sql.group(0)
+    date_sql = date_sql.split(u" ")[0]
+    if date_sql < "2016-01-01":
+        date_sql = "2016-01-01"
+    return date_sql
 
 
 def get_table(sql):
@@ -33,6 +45,13 @@ def transfer_group_by(group_by, table):
     group_by = group_by.group(1)
     group_by = transfer_src(group_by, table)
     return "group by {}\n".format(group_by)
+
+
+def transfer_where(where, table):
+    cond = where.group(1)
+    field = where.group(2)
+    field = transfer_src(field, table)
+    return "{} {}".format(cond, field)
 
 
 def transfer_field(field, table):
@@ -55,16 +74,18 @@ def transfer_dst(dst):
     return '"{}"'.format(dst)
 
 
-# test_sql = u'''SELECT day AS day,
-#               SUM(click) AS sum_click
-# FROM DSP_ANALYSE_D
-# GROUP BY day
-# ORDER BY sum_click DESC LIMIT 50000'''
+test_sql = u'''SELECT cat_name AS cat_name,
+       SUM(click) AS sum__click
+FROM DSP_ANALYSE_D
+WHERE day >= '1917-03-06 14:38:49'
+  AND day <= '2017-03-13 14:38:49'
+GROUP BY cat_name
+ORDER BY sum__click DESC LIMIT 50000'''
+
+test_sql = transfer_sql(test_sql)
+
+print(test_sql)
 #
-# test_sql = transfer_sql(test_sql)
-#
-# print(test_sql)
-#
-# items = re.findall(group_py_pattern, test_sql)
+# items = re.findall(where_pattern, test_sql)
 # for item in items:
 #     print(item)
